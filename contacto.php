d1fe8a042ef41214478bd60ecda629c0e12f49b1
AndresLupian <lupianro@gmail.com>
2021-11-26 00:22:15 -0600

    Agregando Footer PHP

--- contacto.php
+++ contacto.php
@@ -69,20 +69,6 @@
             <input class="boton-verde" type="submit" value="Enviar" id="btnEnviar">
         </form>
     </main>
+    <?php include 'includes/templates/footer.php'; ?>
 
-    <footer class="footer seccion">
-        <div class="contenedor contenedor-footer">
-            <nav class="navegacion">
-                <a href="nosotros.php">Nosotros</a>
-                <a href="anuncios.php">Anuncios</a>
-                <a href="blog.php">Blog</a>
-                <a href="contacto.php">Contacto</a>
-            </nav>
-        </div>
-
-        <p class="copyright">Todos los Derechos Reservados 2021 &copy;</p>
-    </footer>
-
-    <script src="build/js/bundle.min.js"></script>
-</body>
-</html>
+  
--- entrada.php
+++ entrada.php
@@ -16,20 +16,6 @@
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad obcaecati sequi quia quis minus tenetur adipisci a illo nam, saepe fuga cumque dignissimos amet ex quod enim consectetur dolore rerum, iste earum! Voluptatem vitae blanditiis earum quasi aliquid officiis, totam quae ipsam similique qui enim quis quidem! Quos, omnis. Eos consequuntur voluptatem ratione sit delectus eligendi consequatur, atque libero, sequi error minima. Accusantium fuga dolor veritatis repudiandae minus cupiditate tempore pariatur earum aliquid, quo facere, magni officia voluptas iusto quisquam.</p>
         </div>
     </main>
+    <?php include 'includes/templates/footer.php'; ?>
 
-    <footer class="footer seccion">
-        <div class="contenedor contenedor-footer">
-            <nav class="navegacion">
-                <a href="nosotros.php">Nosotros</a>
-                <a href="anuncios.php">Anuncios</a>
-                <a href="blog.php">Blog</a>
-                <a href="contacto.php">Contacto</a>
-            </nav>
-        </div>
-
-        <p class="copyright">Todos los Derechos Reservados 2021 &copy;</p>
-    </footer>
-
-    <script src="build/js/bundle.min.js"></script>
-</body>
-</html>
+  
